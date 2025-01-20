93f5a9ea7f399d64660c0e428b79ce4a6d8f856a
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-01-20 10:45:49 +0100

    feat: card selection

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,27 @@
+import { useState } from "react"
+
 const Main = (prop) => {
+  const [selectedLanguage, setSelectedLanguage] = useState(1)
   const languagesList = prop.languagesList
+
+
+  // }
   return (
     <main>
       <div className="container">
         <div className="buttons">
+
           <ul>
             {languagesList.map(item => (
               <li key={item.id}>
-                <button>{item.title}</button>
+                <button onClick={() => setSelectedLanguage(item)}>{item.title}</button>
               </li>
             ))}
           </ul>
         </div>
         <div className="card">
-          <h4>{languagesList[0].title}</h4>
-          <p>{languagesList[0].description}</p>
+          <h4>{selectedLanguage.title}</h4>
+          <p>{selectedLanguage.description}</p>
         </div>
       </div>
 
